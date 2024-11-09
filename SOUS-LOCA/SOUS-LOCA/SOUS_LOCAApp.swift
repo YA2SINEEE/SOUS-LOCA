1b6d31a83315f5374386aade94a598c2ec0e14ac
YA2SINEEE <flexi.consulting1@gmail.com>
2024-11-09 17:56:04 +0100

    Add projet Xcode

---
+++ SOUS-LOCA/SOUS-LOCA/SOUS_LOCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOUS_LOCAApp.swift
+//  SOUS-LOCA
+//
+//  Created by Yassine Kaddouri on 09/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SOUS_LOCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SOUS-LOCA/SOUS-LOCATests/SOUS_LOCATests.swift
@@ -0,0 +1,16 @@
+//
+//  SOUS_LOCATests.swift
+//  SOUS-LOCATests
+//
+//  Created by Yassine Kaddouri on 09/11/2024.
+//
+
+import Testing
+
+struct SOUS_LOCATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
