ff93addd9744e93d53cd8bcdb3d8974689245174
YA2SINEEE <flexi.consulting1@gmail.com>
2024-11-10 02:21:28 +0100

    Add Case Annonce

--- SOUS-LOCA/SOUS-LOCA/ViewControllers/AnnouncementsView.swift
+++ SOUS-LOCA/SOUS-LOCA/ViewControllers/AnnouncementsView.swift
@@ -10,7 +10,10 @@ struct AnnouncementsView: View {
         VStack {
             // Affichage des annonces avec un HStack sans ScrollView (désactivation du scroll manuel)
             GeometryReader { geometry in
-                HStack(spacing: geometry.size.width * 0.2) { // Ajustement dynamique de l'espacement (20% de la largeur de l'écran)
+                let cardWidth = geometry.size.width * 0.85 // 85% de la largeur de l'écran pour chaque carte
+                let spacing = geometry.size.width * 0.10 // Espacement entre les cartes (10% de la largeur de l'écran)
+                
+                HStack(spacing: spacing) { // Ajustement dynamique de l'espacement
                     // Pour chaque annonce, nous affichons une carte
                     ForEach(announcements.indices, id: \.self) { index in
                         let announcement = announcements[index]
@@ -34,7 +37,7 @@ struct AnnouncementsView: View {
                                         .frame(width: 100, height: 100)
                                 }
                             }
-                            .frame(width: 280, height: 150) // La taille de chaque carte
+                            .frame(width: cardWidth, height: 150) // Taille dynamique des cartes
                             .background(
                                 RoundedRectangle(cornerRadius: 15)
                                     .fill(LinearGradient(gradient: Gradient(colors: [Color("Color1"), Color("Color2")]), startPoint: .bottomLeading, endPoint: .topTrailing))
@@ -44,8 +47,8 @@ struct AnnouncementsView: View {
                         .padding(.vertical, 10)
                     }
                 }
-                .padding(.horizontal, (geometry.size.width - 280) / 2) // Centrer la carte dans la vue
-                .offset(x: CGFloat(currentIndex) * -(geometry.size.width * 0.2 + 280)) // Décalage ajusté pour l'espacement
+                .padding(.horizontal, (geometry.size.width - cardWidth) / 2) // Centrer la carte dans la vue
+                .offset(x: CGFloat(currentIndex) * -(spacing + cardWidth)) // Décalage ajusté pour l'espacement
                 .animation(.easeInOut(duration: 1), value: currentIndex) // Animation du défilement
             }
             .frame(height: 150) // Limiter la hauteur de l'affichage des annonces
