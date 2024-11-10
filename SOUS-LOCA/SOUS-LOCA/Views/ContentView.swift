15ff4bcfb44d6a76d1f4f0fb4070b2d6a14cb931
YA2SINEEE <flexi.consulting1@gmail.com>
2024-11-10 01:08:57 +0100

    TabBar

--- SOUS-LOCA/SOUS-LOCA/Views/ContentView.swift
+++ SOUS-LOCA/SOUS-LOCA/Views/ContentView.swift
@@ -1,14 +1,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tabSelection = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView(selection: $tabSelection) {
+            Text("Tab Content 1")
+                .tag(1)
+            Text("Tab Content 2")
+                .tag(2)
+            Text("Tab Content 3")
+                .tag(3)
+            Text("Tab Content 4")
+                .tag(4)
+            Text("Tab Content 5")
+                .tag(5)
         }
-        .padding()
+        .overlay(alignment: .bottom) {
+            CustomTabView(tabSelection: $tabSelection)
+        }
     }
 }
 
---
+++ SOUS-LOCA/SOUS-LOCA/Views/CustomTabView.swift
@@ -0,0 +1,77 @@
+import SwiftUI
+
+struct CustomTabView: View {
+    @Binding var tabSelection: Int
+    @Namespace private var animationNamespace
+    
+    let tabBarItems: [(image: String, title: String)] = [
+        ("house","Accueil"),
+        ("book","Actualités"),
+        ("video","Formation"),
+        ("person","Contact"),
+        ("gear","Paramètres")
+    ]
+    
+    var body: some View {
+        ZStack {
+            Capsule()
+                .fill(
+                    LinearGradient(
+                        gradient: Gradient(colors: [
+                            Color("Color1"), // En bas à gauche
+                            Color("Color2"),
+                            Color("Color3"),
+                            Color("Color4")  // En haut à droite
+                        ]),
+                        startPoint: .bottomLeading,
+                        endPoint: .topTrailing
+                    )
+                )
+                .frame(height: 80)
+                //.foregroundColor(Color(.secondarySystemBackground))
+                .shadow(radius: 8) //2
+            
+            HStack {
+                ForEach(tabBarItems.indices, id: \.self) { index in
+                    let isSelected = (index + 1 == tabSelection)
+                    
+                    Button(action: {
+                        tabSelection = index + 1
+                    }) {
+                        VStack (spacing: 8) {
+                            Spacer()
+                            
+                            Image(systemName: tabBarItems[index].image)
+                            
+                            Text(tabBarItems[index].title)
+                                .font(.caption)
+                            
+                            if isSelected {
+                                Capsule()
+                                    .frame(height: 8)
+                                    .foregroundColor(.white)
+                                    .matchedGeometryEffect(id: "SelectedTabId", in: animationNamespace)
+                                    .offset(y: 3)
+                            } else {
+                                Capsule()
+                                    .frame(height: 8)
+                                    .foregroundColor(.clear)
+                                    .offset(y: 3)
+                            }
+                        }
+                        .foregroundColor(isSelected ? .white : .gray)
+                    }
+                }
+            }
+            .frame(height: 80)
+            .clipShape(Capsule())
+        }
+        .padding(.horizontal)
+    }
+}
+
+#Preview {
+    CustomTabView(tabSelection: .constant(1))
+        .previewLayout(.sizeThatFits)
+        .padding(.vertical)
+}
